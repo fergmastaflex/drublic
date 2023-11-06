a602743a6d830b3dbcd2058a0de952bbea8560ed
Joey Ferguson <fergmastaflex@gmail.com>
2023-11-06 11:57:18 -0500

    Add better room and general cleanup

--- scripts/drone.gd
+++ scripts/drone.gd
@@ -10,19 +10,8 @@ var follow_distance : float = 2
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(_delta):
-	# var enemy = get_tree().get_nodes_in_group("enemies")[0]
-	# if enemy != null && position.distance_to(enemy.position) > follow_distance:
-	# 	var dir = (enemy.position - position).normalized()
-		
-	# 	velocity.x = dir.x * SPEED
-	# 	velocity.y = 0
-	# 	velocity.z = dir.z * SPEED
-	# else:
 	var dir = (player.position - position).normalized()
-	if position.distance_to(player.position) > follow_distance: 
-	# 	look_at(player.position)
-
-	
+	if position.distance_to(player.position) > follow_distance:
 		velocity.x = dir.x * SPEED
 		velocity.y = 0
 		velocity.z = dir.z * SPEED
