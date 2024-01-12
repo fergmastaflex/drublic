cbc39b18c44e6c3bc4857e3084f07ef4a4e43b15
Joey Ferguson <fergmastaflex@gmail.com>
2024-01-12 09:59:39 -0500

    Add optics-robotics

--- scripts/weapons/projectiles/optics/optics_projectile.gd
+++ scripts/weapons/projectiles/optics/optics_projectile.gd
@@ -4,8 +4,10 @@ class_name OpticsProjectile
 func _init():
 	bullet_velocity = 100
 	damage = 25
+	projectile_collider = null
 	
 func give_damage(body):
 	if body.is_in_group("enemies") && body.has_method("take_damage") && !body.ally:
 		add_status_effect(body)
 		body.take_damage(damage)
+		
